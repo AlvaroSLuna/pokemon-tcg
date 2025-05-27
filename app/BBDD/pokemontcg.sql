957aef1282e4294d2a5a20d06fed1dbb9d237e78
Álvaro Serrano <serranoalvaroluna@gmail.com>
2025-05-27 18:16:20 +0200

    cambio en BBDD

--- app/BBDD/pokemontcg.sql
+++ app/BBDD/pokemontcg.sql
@@ -19,6 +19,9 @@ SET time_zone = "+00:00";
 
 --
 -- Base de datos: `pokemontcg`
+
+CREATE DATABASE IF NOT EXISTS `pokemontcg` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pokemontcg`;
 --
 
 -- --------------------------------------------------------
